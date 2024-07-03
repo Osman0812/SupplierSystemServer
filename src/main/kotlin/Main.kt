061fd63d46521823ef53712782b8172062cca572
osman <osman.mamedov2000@gmail.com>
2024-07-03 23:06:08 +0300

    fixed reserved days assignment

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,8 +9,6 @@ import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 data class Supplier(val info: String, val type: String, val reservedDays: List<Int>)
 data class Assignment(val dayOfTheMonth: Int, val contractSupplier: String, val stockSupplier: String)
 
@@ -35,34 +33,75 @@ fun main() {
 
 fun makeAssignments(suppliers: List<Supplier>): List<Assignment> {
     val assignments = mutableListOf<Assignment>()
-    val usedSuppliersToday = mutableSetOf<Supplier>()
-    val usedSuppliersYesterday = mutableSetOf<Supplier>()
+    val usedSuppliersToday = mutableListOf<Supplier>()
+    val usedSuppliersYesterday = mutableListOf<Supplier>()
+    println(suppliers.toString())
 
-    for (day in 1..10) {
-        val availableContractSuppliers = suppliers.filter { it.type == "Contract" || it.type == "Both" }
-            .filter { it !in usedSuppliersToday }
-        val availableStockSuppliers = suppliers.filter { it.type == "Stock" || it.type == "Both" }
-            .filter { it !in usedSuppliersToday }
-
-        if (availableContractSuppliers.isNotEmpty() && availableStockSuppliers.isNotEmpty()) {
-            val contractSupplier = availableContractSuppliers.random()
-            val stockSupplier = availableStockSuppliers.random()
-            assignments.add(Assignment(day, contractSupplier.info, stockSupplier.info))
-            usedSuppliersToday.add(contractSupplier)
-            usedSuppliersToday.add(stockSupplier)
-        } else if (availableContractSuppliers.isNotEmpty() && availableStockSuppliers.isEmpty()) {
-            val contractSupplier = availableContractSuppliers.random()
-            assignments.add(Assignment(day, contractSupplier.info, contractSupplier.info))
-            usedSuppliersToday.add(contractSupplier)
-        } else if (availableContractSuppliers.isEmpty() && availableStockSuppliers.isNotEmpty()) {
-            val stockSupplier = availableStockSuppliers.random()
-            assignments.add(Assignment(day, stockSupplier.info, stockSupplier.info))
-            usedSuppliersToday.add(stockSupplier)
+    for (day in 1..30) {
+        val availableContractSuppliers = suppliers.filter {
+            (it.type == "Contract" || it.type == "Both") &&
+                    it !in usedSuppliersToday &&
+                    it !in usedSuppliersYesterday &&
+                    day !in it.reservedDays
         }
 
+        val availableStockSuppliers = suppliers.filter {
+            (it.type == "Stock" || it.type == "Both") &&
+                    it !in usedSuppliersToday &&
+                    it !in usedSuppliersYesterday &&
+                    day !in it.reservedDays
+        }
+
+        if (availableContractSuppliers.isNotEmpty()) {
+            val contractSupplier = availableContractSuppliers.random()
+            if (availableStockSuppliers.isNotEmpty()) {
+                val stockSupplier = availableStockSuppliers.random()
+                assignments.add(Assignment(day, contractSupplier.info, stockSupplier.info))
+                usedSuppliersToday.add(contractSupplier)
+                usedSuppliersToday.add(stockSupplier)
+            } else {
+                val possibleStockSuppliers = suppliers.filter {
+                    (it.type == "Stock" || it.type == "Both") &&
+                            it !in usedSuppliersToday &&
+                            it != contractSupplier &&
+                            day !in it.reservedDays
+                }
+
+                if (possibleStockSuppliers.isNotEmpty()) {
+                    val stockSupplier = possibleStockSuppliers.random()
+                    assignments.add(Assignment(day, contractSupplier.info, stockSupplier.info))
+                    usedSuppliersToday.add(contractSupplier)
+                    usedSuppliersToday.add(stockSupplier)
+                } else {
+                    assignments.add(Assignment(day, contractSupplier.info, contractSupplier.info))
+                    usedSuppliersToday.add(contractSupplier)
+                }
+            }
+        } else {
+            if (availableStockSuppliers.isNotEmpty()) {
+                val stockSupplier = availableStockSuppliers.random()
+                val possibleContractSuppliers = suppliers.filter {
+                    (it.type == "Contract" || it.type == "Both") &&
+                            it !in usedSuppliersToday &&
+                            it != stockSupplier &&
+                            day !in it.reservedDays
+                }
+
+                if (possibleContractSuppliers.isNotEmpty()) {
+                    val contractSupplier = possibleContractSuppliers.random()
+                    assignments.add(Assignment(day, contractSupplier.info, stockSupplier.info))
+                    usedSuppliersToday.add(contractSupplier)
+                    usedSuppliersToday.add(stockSupplier)
+                } else {
+                    assignments.add(Assignment(day, stockSupplier.info, stockSupplier.info))
+                    usedSuppliersToday.add(stockSupplier)
+                }
+            }
+        }
         usedSuppliersYesterday.clear()
         usedSuppliersYesterday.addAll(usedSuppliersToday)
         usedSuppliersToday.clear()
     }
     return assignments
 }
+
